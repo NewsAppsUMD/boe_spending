e8d45e6c69cd7463f8f0e26620de7f3115c328c6
Derek Willis <dwillis@gmail.com>
2023-06-01 11:10:01 -0400

    latest release

--- boe_setup.sh
+++ boe_setup.sh
@@ -1,7 +1,6 @@
 rm -f boe_spending.db
 pip install sqlite-utils datasette
-datasette install datasette-codespaces
 sqlite-utils insert boe_spending.db vendors boe_spending_cleaned.csv --csv
-sqlite-utils transform boe_spending.db vendors --type Amount float
+sqlite-utils transform boe_spending.db vendors --type amount float
 sqlite-utils enable-fts boe_spending.db vendors payee_name purpose_of_payment_baltimore_county_only
 datasette publish heroku boe_spending.db -n md-boe-spending
